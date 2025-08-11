dc4b7fa3784eda5025ea0481fcb8e385bdbb4f44
IgorKaisarov <Igo444@mail.ru>
2025-08-11 20:14:27 +0600

    Task: 1 2 3

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,40 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!");
+        System.out.printf(" Home Work 7 ");
 
+// Task 1
+        System.out.println(" Task 1 ");
 
-
+        int cash = 15000;
+        int total = 0;
+        int month = 0;
+        while (total < 2_459_000) {
+            total = total + cash;
+            month++;
+            System.out.println(" Месяц " + month + " сумма накоплений равна " + total + " рублей. ");
         }
+
+        // Task 2
+        System.out.println(" Task 2 ");
+
+        int first = 1;
+        while (first <= 10) {
+            System.out.print(first + " ");
+            first ++;
+        }
+
+        System.out.println();
+
+        for (int second = 10; second > 0; second--) {
+            System.out.print(second + " ");
+        }
+
+        // Task 3
+        System.out.println(" Task 3 ");
+
+
+
+
     }
+}
